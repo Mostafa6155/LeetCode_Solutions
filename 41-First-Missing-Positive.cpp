06a185ed142900158903e7f137c36a47afc6abbf
Mostafa6155 <104218923+Mostafa6155@users.noreply.github.com>
2024-03-26 14:25:42 +0200

    [41] [Time Beats: 17.56%] [Memory Beats: 71.13%] - LeetPush

--- 41-First-Missing-Positive.cpp
+++ 41-First-Missing-Positive.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-    int n = nums.size();
-
-    int j = 0;
-    for (int i = 0; i < n; ++i) {
-        if (nums[i] <= 0) {
-            swap(nums[i], nums[j]);
-            ++j;
+    sort(nums.begin(),nums.end());
+    vector<int> v(nums.size(),0);
+    v[0]=1;
+    for (int i = 1; i < v.size(); ++i) {
+        if (nums[i] != nums[i-1]){
+            v[i]++;
         }
     }
-    for (int i = j; i < n; ++i) {
-        int num = abs(nums[i]);
-        if (num <= n - j) {
-            nums[num + j - 1] = -abs(nums[num + j - 1]);
+    int i=0;
+    int val=1;
+    while (true){
+        if (nums[i] > 0 && v[i]){
+            if (nums[i] != val){
+                return val;
+            }
+            val++;
+        }
+        i++;
+        if (i==nums.size()){
+            break;
         }
     }
-    for (int i = j; i < n; ++i) {
-        if (nums[i] > 0) {
-            return i - j + 1;
-        }
-    }
-
-    return n - j + 1;
+    return val;
 }
-
 };
