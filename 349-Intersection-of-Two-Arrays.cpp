674325f3a330f9c4888a35025f01558fb8df11c8
Mostafa6155 <104218923+Mostafa6155@users.noreply.github.com>
2024-04-04 18:12:08 +0200

    [349] [Time Beats: 61.51%] [Memory Beats: 100.00%] - LeetPush

---
+++ 349-Intersection-of-Two-Arrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    set<int> v;
+    int i=0,j=0;
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    while (i<nums1.size() && j<nums2.size()){
+        if (nums1[i]==nums2[j]){
+            v.insert(nums1[i]);
+            i++;
+            j++;
+        }
+        else if (nums1[i] < nums2[j]){
+            i++;
+        } else{
+            j++;
+        }
+    }
+    vector<int> c(v.begin(),v.end());
+    return c;
+}
+};
