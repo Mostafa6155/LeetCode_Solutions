c21f22cbbe2ca73b4fe0e013d1b4716264d74043
Mostafa6155 <104218923+Mostafa6155@users.noreply.github.com>
2024-04-07 00:00:43 +0200

    [1295] [Time Beats: 5.67%] [Memory Beats: 25.79%] - LeetPush

---
+++ 1295-Find-Numbers-with-Even-Number-of-Digits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            String n=Integer.toString(nums[i]);
+            if(n.length()%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
