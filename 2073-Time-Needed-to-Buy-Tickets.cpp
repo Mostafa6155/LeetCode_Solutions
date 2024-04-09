e3a3624d8152c977c381bb212bc6e9ffd2748170
Mostafa6155 <104218923+Mostafa6155@users.noreply.github.com>
2024-04-09 22:25:27 +0200

    [2073] [Time Beats: 100.00%] [Memory Beats: 40.86%] - LeetPush

--- 2073-Time-Needed-to-Buy-Tickets.cpp
+++ 2073-Time-Needed-to-Buy-Tickets.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-    int time=0;
-    while (tickets[k] > 0){
-        for (int i = 0; i < tickets.size(); ++i) {
-            if (tickets[i] > 0){
-                time++;
-                tickets[i]--;
-                if (tickets[k]==0){
-                    break;
-                }
-            }
+    int time = 0;
+    std::multiset<int> ms;
+    int before = 0;
+    for (int i = 0; i < tickets.size(); i++) {
+        if (tickets[i] < tickets[k]) {
+            ms.insert(tickets[i]);
+        }
+        if (i < k && tickets[i] >= tickets[k]) {
+            before++;
         }
     }
+    int n = tickets.size();
+    auto it = ms.begin();
+    int last = 0;
+    for (int i = 0; it != ms.end() && !ms.empty(); ++i) {
+        time += (*it - last) * n;
+        n--;
+        last = *it;
+        it++;
+    }
+    time += (tickets[k] - last - 1) * n;
+    time += (before + 1);
     return time;
 }
 };
