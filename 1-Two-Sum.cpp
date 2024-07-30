ee1ffe0f0cbe75f88a62ac8f0f2c351126c07257
Mostafa6155 <104218923+Mostafa6155@users.noreply.github.com>
2024-07-30 13:20:58 +0300

    [1] [Time Beats: 84.92%] [Memory Beats: 47.74%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    v.push_back(i);
-                    v.push_back(j);
-                }
-            }
-        }
-        return v;
+    vector<int> twoSum(vector<int>& nums, int target){
+    vector<pair<int, int>> v(nums.size());
+    for (int i = 0; i < v.size(); ++i) {
+        v[i].first=nums[i];
+        v[i].second = i ;
     }
+    sort(v.begin(),v.end());
+    vector<int> res(0);
+    int i=0;int j=v.size()-1;
+    while (i < j){
+        if (v[i].first + v[j].first > target){
+            j--;
+        }else if(v[i].first + v[j].first < target){
+            i++;
+        } else{
+            res.push_back(v[i].second);
+            res.push_back(v[j].second);
+            i++;
+        }
+    }
+    return res;
+}
 };
